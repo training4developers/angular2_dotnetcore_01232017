9b28ae2cb9a0b1a0003b4c488aa95f210be17ec2
Eric W. Greene <eric@ericwgreene.com>
2017-01-23 14:21:19 -0500

    added code demos

--- wwwrootsrc/ts/app/app.component.ts
+++ wwwrootsrc/ts/app/app.component.ts
@@ -2,8 +2,51 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: `<h1>{{message}}</h1>`,
+    template: `
+        <h1>{{message}}</h1>
+        <ul>
+            <li *ngFor="let color of colors">{{color.label}} - {{color.code}}</li>
+        </ul>
+        <form>
+            <div>
+                <label for="new-color-code-input">New Color Code</label>
+                <input type="text" id="new-color-code-input"
+                    name="newColorCodeInput"
+                    [(ngModel)]="newColor.code">
+            </div>
+            <div>
+                <label for="new-color-label-input">New Color Label</label>
+                <input type="text" id="new-color-label-input"
+                    name="newColorLabelInput"
+                    [(ngModel)]="newColor.label">
+            </div>
+            <button type="button" (click)="addColor()">Add Color</button>
+        </form>
+    `,
 })
 export class AppComponent {
     public message: string = "Hello World!";
+    public newColor: any = {
+        code: "",
+        label: "",
+    };
+
+    public colors: any[] = [
+        { code: "green", label: "Green" },
+        { code: "yellow", label: "Yellow" },
+        { code: "red", label: "Red" },
+        { code: "gold", label: "Gold" },
+        { code: "white", label: "White" },
+        { code: "saffron", label: "Saffron" },
+        { code: "blue", label: "Blue" },
+    ];
+
+    public addColor() {
+        this.colors.push(this.newColor);
+        this.newColor = {};
+    }
 }
+
+// <template ngFor let-color [ngForOf]="colors">
+//     <li>{{color}}</li>
+// </template>
