2e9c00eeddabfbb43bb43f1c58b56a8f3ed054cf
Eric W. Greene <eric@ericwgreene.com>
2017-01-25 14:03:31 -0500

    added code demos

--- wwwrootsrc/ts/app/components/filtered-item-list.component.ts
+++ wwwrootsrc/ts/app/components/filtered-item-list.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input, OnInit } from "@angular/core";
 
+import { ColorsSvc } from "../services/colors-svc";
+
 @Component({
+    providers: [ ColorsSvc ],
     selector: "filtered-item-list",
     template: `
         <filter-item-form
             (newFilterValue)="receivedNewFilterValue($event)">
         </filter-item-form>
         <item-list [items]="filteredItems"></item-list>
-    `
+    `,
 })
 export class FilteredItemList implements OnInit {
 
@@ -15,13 +18,20 @@ export class FilteredItemList implements OnInit {
     public items: string[] = [];
     public filteredItems: string[] = [];
 
+    constructor(private colorsSvc: ColorsSvc) { }
+
+    // private colorsSvc: ColorsSvc;
+    // constructor(colorsSvc: ColorsSvc) {
+    //     this.colorsSvc = colorsSvc;
+    // }
+
     public receivedNewFilterValue(filterValue: string) {
         this.filteredItems = this.items.filter((item: string) =>
             item.includes(filterValue));
     }
 
     public ngOnInit() {
-        this.filteredItems = this.items.concat();
+        this.items = this.filteredItems = this.colorsSvc.getColors();
     }
 
 }
